96e1b57de5fbc86cd1a2e5f1f0dfdd5b8daec6d8
Me-coder2024 <me.coder.in@gmail.com>
2025-01-08 18:45:58 +0530

    Initial Commit

---
+++ Fooddelivery/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fooddelivery
+//
+//  Created by Teacher on 07/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            
+            ZStack{
+                
+                VStack{
+                   Text("Helloo world")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fooddelivery/FooddeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooddeliveryApp.swift
+//  Fooddelivery
+//
+//  Created by Teacher on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FooddeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
